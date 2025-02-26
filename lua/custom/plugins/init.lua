3f0e66db3701952e4909293a7fce9d6340375a34
Maksims Orlovs <m.orlov10@gmail.com>
2025-02-26 18:19:39 +0100

    add local LLM code completion

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -165,39 +165,99 @@ return {
   --   end,
   -- },
   {
-    'Exafunction/codeium.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'hrsh7th/nvim-cmp',
-    },
-    config = function()
-      local codeium = require 'codeium'
-      local cfg = {
-        enable_chat = false,
-        enable_cmp_source = false,
-        virtual_text = {
-          enabled = true,
-          idle_delay = 100,
-          key_bindings = {
-            accept = '<M-j>',
-            accept_word = false,
-            accept_line = '<M-J>',
-            next = '<M-]>',
-            prev = '<M-[>',
-          },
-        },
-      }
-      codeium.setup(cfg)
+    'ghillb/llama.vim',
 
-      vim.g.codeium_enabled = true
-      vim.keymap.set('n', '<leader>cl', function()
-        vim.g.codeium_enabled = not vim.g.codeium_enabled
-        cfg.virtual_text.enabled = vim.g.codeium_enabled
-        codeium.setup(cfg)
-        print('Codeium ' .. (vim.g.codeium_enabled and 'Enabled' or 'Disabled'))
-      end, { desc = 'Toggle Ze [L]LMs' })
+    init = function()
+      vim.g.llama_config = {
+        endpoint = 'http://localhost:8000/infill',
+        show_info = 0,
+      }
+
+      -- Check if the container is running
+      local handle = io.popen "docker ps --filter 'name=ze-llm' --format '{{.ID}}'"
+      if handle == nil then
+        vim.notify('Skill issue', vim.log.levels.ERROR)
+        vim.g.llm_enabled = false
+        return
+      end
+
+      local result = handle:read '*a'
+      handle:close()
+      if result == '' then
+        vim.notify('Starting ze LLMs', vim.log.levels.INFO)
+        local job_id = vim.fn.jobstart('docker start ze-llm', {
+          detach = true,
+          on_exit = function(_, code)
+            if code == 0 then
+              vim.notify('Ze LLMs are online', vim.log.levels.INFO)
+              vim.g.llm_enabled = true
+            else
+              vim.notify('Ze LLMs failed to start', vim.log.levels.ERROR)
+              vim.g.llm_enabled = false
+            end
+          end,
+        })
+      else
+        vim.notify('Ze LLMs are online', vim.log.levels.INFO)
+        vim.g.llm_enabled = true
+      end
+
+      -- Stop the container when Neovim exits
+      vim.api.nvim_create_autocmd('VimLeavePre', {
+        callback = function()
+          vim.notify('Stopping ze LLMs', vim.log.levels.INFO)
+          vim.fn.jobstart('docker stop ze-llm', {
+            detach = true,
+          })
+        end,
+      })
     end,
+
+    keys = {
+      {
+        '<leader>cl',
+        function()
+          vim.g.llm_enabled = not vim.g.llm_enabled
+          print((vim.g.llm_enabled and 'Enabled' or 'Disabled') .. ' ze LLMs')
+        end,
+        desc = 'Toggle Ze [L]LMs',
+      },
+    },
   },
+  -- {
+  --   'Exafunction/codeium.nvim',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'hrsh7th/nvim-cmp',
+  --   },
+  --   config = function()
+  --     local codeium = require 'codeium'
+  --     local cfg = {
+  --       enable_chat = false,
+  --       enable_cmp_source = false,
+  --       virtual_text = {
+  --         enabled = true,
+  --         idle_delay = 100,
+  --         key_bindings = {
+  --           accept = '<M-j>',
+  --           accept_word = false,
+  --           accept_line = '<M-J>',
+  --           next = '<M-]>',
+  --           prev = '<M-[>',
+  --         },
+  --       },
+  --     }
+  --     codeium.setup(cfg)
+  --
+  --     vim.g.codeium_enabled = true
+  --     vim.keymap.set('n', '<leader>cl', function()
+  --       vim.g.codeium_enabled = not vim.g.codeium_enabled
+  --       cfg.virtual_text.enabled = vim.g.codeium_enabled
+  --       codeium.setup(cfg)
+  --       print('Codeium ' .. (vim.g.codeium_enabled and 'Enabled' or 'Disabled'))
+  --     end, { desc = 'Toggle Ze [L]LMs' })
+  --   end,
+  -- },
   {
     'declancm/cinnamon.nvim',
     version = '*', -- use latest release
